8719a3590dc5194c729c9d3d36115ac4d898dc6d
NertNiels <nertniels@gmail.com>
2019-03-10 11:09:33 +0100

    Edited images glitch

--- carrie_demo/assets/images.js
+++ carrie_demo/assets/images.js
@@ -25,28 +25,28 @@ var beware;
 var carrie_enjoy_tea;
 
 function setupImages() {
-  carrie_hi = loadImage("/assets/carrie_hi.png")
-  carrie_angry = loadImage("/assets/carrie_angry.png");
-  carrie_smile = loadImage("/assets/carrie_smile.png");
-  carrie_happy = loadImage("/assets/carrie_happy.png");
-  drink = loadImage("/assets/drink.png");
-  food = loadImage("/assets/food.png");
-  clock = loadImage("/assets/clock.png");
-  pan = loadImage("/assets/pan.png");
-  snack = loadImage("/assets/snack.png");
-  tea = loadImage("/assets/tea.png");
-  hands_down_left = loadImage("/assets/hands_down_left.png");
-  hands_down_right = loadImage("/assets/hands_down_right.png");
-  hands_up_left = loadImage("/assets/hands_up_left.png");
-  hands_up_right = loadImage("/assets/hands_up_right.png");
-  exclamationmark = loadImage("/assets/exclamationmark.png");
-  questionmark = loadImage("/assets/questionmark.png");
-  fire = loadImage("/assets/fire.png");
-  water = loadImage("/assets/water.png");
-  waterboiler_fill = loadImage("/assets/waterboiler_fill.png");
-  waterboiler_electrify = loadImage("/assets/waterboiler_electrify.png");
-  waterboiler_pouring = loadImage("/assets/waterboiler_pouring.png");
-  tea_put_teabag_in = loadImage("/assets/tea_put_teabag_in.png");
-  beware = loadImage("/assets/beware.png");
-  carrie_enjoy_tea = loadImage("/assets/carrie_enjoy_tea.png");
+  carrie_hi = loadImage("/carrie_demo/assets/carrie_hi.png")
+  carrie_angry = loadImage("/carrie_demo/assets/carrie_angry.png");
+  carrie_smile = loadImage("/carrie_demo/assets/carrie_smile.png");
+  carrie_happy = loadImage("/carrie_demo/assets/carrie_happy.png");
+  drink = loadImage("/carrie_demo/assets/drink.png");
+  food = loadImage("/carrie_demo/assets/food.png");
+  clock = loadImage("/carrie_demo/assets/clock.png");
+  pan = loadImage("/carrie_demo/assets/pan.png");
+  snack = loadImage("/carrie_demo/assets/snack.png");
+  tea = loadImage("/carrie_demo/assets/tea.png");
+  hands_down_left = loadImage("/carrie_demo/assets/hands_down_left.png");
+  hands_down_right = loadImage("/carrie_demo/assets/hands_down_right.png");
+  hands_up_left = loadImage("/carrie_demo/assets/hands_up_left.png");
+  hands_up_right = loadImage("/carrie_demo/assets/hands_up_right.png");
+  exclamationmark = loadImage("/carrie_demo/assets/exclamationmark.png");
+  questionmark = loadImage("/carrie_demo/assets/questionmark.png");
+  fire = loadImage("/carrie_demo/assets/fire.png");
+  water = loadImage("/carrie_demo/assets/water.png");
+  waterboiler_fill = loadImage("/carrie_demo/assets/waterboiler_fill.png");
+  waterboiler_electrify = loadImage("/carrie_demo/assets/waterboiler_electrify.png");
+  waterboiler_pouring = loadImage("/carrie_demo/assets/waterboiler_pouring.png");
+  tea_put_teabag_in = loadImage("/carrie_demo/assets/tea_put_teabag_in.png");
+  beware = loadImage("/carrie_demo/assets/beware.png");
+  carrie_enjoy_tea = loadImage("/carrie_demo/assets/carrie_enjoy_tea.png");
 }
